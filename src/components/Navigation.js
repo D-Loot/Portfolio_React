6ac5ba4e62c2dd054c973ba1c1af1a367358f0ab
Dan Looten <danwlooten@gmail.com>
2021-09-27 02:20:29 -0500

    add navigation to switch between pages

---
+++ src/components/Navigation.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function NavigationTabs({ currentPage, handlePageChange }) {
+  return (
+    <div className="tabs-div">
+      <p
+        onClick={() => handlePageChange("About")}
+        className={currentPage === "About" ? "clicked" : "hover-text"}
+      >
+        About Me
+      </p>
+      <p
+        onClick={() => handlePageChange("Portfolio")}
+        className={currentPage === "Portfolio" ? "clicked" : "hover-text"}
+      >
+        Portfolio
+      </p>
+      <p
+        onClick={() => handlePageChange("Contact")}
+        className={currentPage === "Contact" ? "clicked" : "hover-text"}
+      >
+        Contact Me
+      </p>
+      <p
+        // href="./assets/LOOTEN - RESUME.pdf"
+        onClick={() => handlePageChange("Resume")}
+        className={currentPage === "Resume" ? "clicked" : "hover-text"}
+      >
+        Resume
+      </p>
+    </div>
+  );
+}
+
+NavigationTabs.propTypes = {
+  handler: PropTypes.func.isRequired,
+  currentPage: PropTypes.string,
+  handlePageChange: PropTypes.string,
+};
+
+NavigationTabs.defaultProps = {
+  currentPage: "Portfolio",
+};
+
+export default NavigationTabs;
