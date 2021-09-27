07d986bda53bbe2f09506738804be7cb16dbb451
Dan Looten <danwlooten@gmail.com>
2021-09-27 04:23:09 -0500

    Update Resume page with lists and PDF

--- src/components/tabs/Resume.js
+++ src/components/tabs/Resume.js
@@ -2,24 +2,45 @@ import React from "react";
 
 export default function Resume() {
   return (
-    <section id="about-me logo" className="content-grid grid-area-1">
-      <h2>Resume</h2>
-      <a href="./assets/LOOTEN - RESUME.pdf">Resume PDF</a>
-      <ul>
-        <li></li>
-        Dan Looten Web Development offers expertise in both front-end and
-        back-end web development. <br /> <br />
-        If you are a small company looking for website online to host web sales
-        or a large corporation <br />
-        looking for back-end assistance, Dan Looten Web Development can help
-        you.
-      </ul>
+    <section id="about-me logo" className="content-grid grid-area-2">
+      <h2 className="contentTitle resumeTitle">
+        <a href="./assets/LOOTEN - RESUME.pdf" target="_blank">
+          Resume PDF
+        </a>
+      </h2>
+      <div className="resumeDiv">
+        <div>
+          <h3 className="resumeListTitle">Languages</h3>
+          <ul className="resumeList">
+            <li>React</li>
+            <li>MongoDB</li>
+            <li>ExpressJS</li>
+            <li>NodeJS</li>
+            <li>MySQL</li>
+            <li>JavaScript ES6+</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>SQL</li>
+            <li>Visual Basic</li>
+            <li>Python</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="resumeListTitle">Applications</h3>
+          <ul className="resumeList">
+            <li>GitHub</li>
+            <li>Heroku</li>
+            <li>MySQL</li>
+            <li>VSCode</li>
+          </ul>
 
-      <img
-        src="./assets/images/cover photo.jpg"
-        alt="Profile"
-        className="profile-pic"
-      />
+          <h3 className="resumeListTitle">Frameworks</h3>
+          <ul className="resumeList">
+            <li>BootStrap</li>
+            <li>jQuery</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
