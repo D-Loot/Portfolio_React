99c9ade39b05ee702a8b0c9c9e16ccad544decf7
Dan Looten <danwlooten@gmail.com>
2021-09-27 02:15:45 -0500

    Add a header section to display on each page

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function Header() {
+  return (
+    <div className="head-block">
+      <h1>Dan Looten Web Development</h1>
+    </div>
+  );
+}
+
+export default Header;
