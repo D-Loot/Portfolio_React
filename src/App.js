a83da412367f8c1d9196e370790de4cf0bb1a649
Dan Looten <danwlooten@gmail.com>
2021-09-27 02:11:33 -0500

    add functionality to app.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import ContentContainer from "./components/ContentContainer";
+import Footer from "./components/Footer";
+
 function App() {
-  return <main />;
+  return (
+    <main className="flex flex-col h-screen justify-between">
+      <Header />
+      <ContentContainer />
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
