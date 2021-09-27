5cac09a0186641b8cc31fed8bc6fe43658d845ef
Dan Looten <danwlooten@gmail.com>
2021-09-27 02:25:13 -0500

    Add Resume section

---
+++ src/components/tabs/Resume.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <section id="about-me logo" className="content-grid grid-area-1">
+      <h2>Resume</h2>
+      <a href="./assets/LOOTEN - RESUME.pdf">Resume PDF</a>
+      <ul>
+        <li></li>
+        Dan Looten Web Development offers expertise in both front-end and
+        back-end web development. <br /> <br />
+        If you are a small company looking for website online to host web sales
+        or a large corporation <br />
+        looking for back-end assistance, Dan Looten Web Development can help
+        you.
+      </ul>
+
+      <img
+        src="./assets/images/cover photo.jpg"
+        alt="Profile"
+        className="profile-pic"
+      />
+    </section>
+  );
+}
